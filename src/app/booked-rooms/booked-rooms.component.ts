6d71bdfda5368355b4bfdd7f56995b3e242d218f
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-04-10 14:25:35 +0400

    commit first ANG-prj 4/10/2025

---
+++ src/app/booked-rooms/booked-rooms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booked-rooms',
+  imports: [],
+  templateUrl: './booked-rooms.component.html',
+  styleUrl: './booked-rooms.component.scss'
+})
+export class BookedRoomsComponent {
+
+}
